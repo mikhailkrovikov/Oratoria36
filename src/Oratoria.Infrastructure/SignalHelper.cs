2100c255226de46598d1c52ee509d0d31c6bf43f
mkrovikov <mikhailkrovikov@gmail.com>
2026-08-31 11:32:12 +0300

    refact

--- src/Oratoria.Infrastructure/SignalHelper.cs
+++ src/Oratoria.Infrastructure/SignalHelper.cs
@@ -5,16 +5,18 @@ namespace Oratoria.Infrastructure
     public static class SignalHelper<T>
         where T : class
     {
+        private static string _field = "DeviceId";
+
         public static T? GetSignal(Enum id, IEnumerable<T> signalCollection, Type signalAttribute)
         {
             var collection = (object)signalCollection;
             var property = collection.GetType()
                 .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .FirstOrDefault(p => p.GetCustomAttributes(inherit: false).Any(a =>
+                .FirstOrDefault(p => p.GetCustomAttributes(false).Any(a =>
                     a.GetType().IsGenericType &&
                     a.GetType().GetGenericTypeDefinition() == signalAttribute &&
                     Equals(
-                        a.GetType().GetField("DeviceId", BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!.GetValue(a),
+                        a.GetType().GetField(_field, BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!.GetValue(a),
                         id)));
             return property?.GetValue(collection) as T;
         }
